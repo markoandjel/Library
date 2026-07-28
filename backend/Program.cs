d03e2f9ca7bd8141103054b8a45974507b6a5aa4
markoandjel <marko.an@outlook.com>
2026-07-28 18:54:11 +0200

    Changed libraryAccessUpload limit to be 2h

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents(options =>
     {
         options.DetailedErrors = builder.Environment.IsDevelopment();
+        options.JSInteropDefaultCallTimeout = TimeSpan.FromHours(2);
     });
 builder.Services.AddMudServices();
 builder.Services.AddEndpointsApiExplorer();
